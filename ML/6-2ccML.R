9fc9eeeecd894ff6a84d4b4b33e6f3c1f8b14464
Scott Boersma <sboersma91@gmail.com>
2020-09-27 14:17:53 -0400

    Larry has come with the heatwave

--- ML/6-2ccML.R
+++ ML/6-2ccML.R
@@ -38,24 +38,63 @@ movielens %>% group_by(movieId) %>%
   coord_trans(y = "sqrt") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+library(dslabs)
+library(tidyverse)
+data("movielens")
+
+head(movielens)
+
+movielens %>%
+  summarize(n_users = n_distinct(userId),
+            n_movies = n_distinct(movieId))
+
+keep <- movielens %>%
+  dplyr::count(movieId) %>%
+  top_n(5) %>%
+  pull(movieId)
+tab <- movielens %>%
+  filter(userId %in% c(13:20)) %>% 
+  filter(movieId %in% keep) %>% 
+  select(userId, title, rating) %>% 
+  spread(title, rating)
+tab %>% knitr::kable()
 
 
+na.rm(tab$`Shawshank Redemption, The`)
 
-p <- w %>% group_by(year)
-p
-w
-identical(w,p)
-
-# %>% 
-#   group_by(year) %>% 
-#   summarize(med = median(n_ratings)) %>% 
-#   arrange(desc(med))
+movielens %>% 
+  filter(title == "Shawshank Redemption, The" & year < "2019") %>% 
+  summarize(avg = mean(rating))
 
 
+movielens %>% 
+  filter(year >= 1993) %>% 
+  group_by(title) %>% 
+  summarize(n_of_ratings = n(), year = year, yeardiff = 2018 - year) %>% 
+  group_by(title, year) %>% 
+  summarize(medians = median(n_of_ratings/yeardiff)) %>% 
+  group_by(title) %>% 
+  arrange(desc(medians)) %>%
+  top_n(25)
 
-z<-movielens %>%
+
+head(movielens)
+
+movielens %>% 
+  filter(year >= 1993) %>%
   group_by(movieId) %>%
-  summarize(n_ratings = sqrt(n()), year = year) %>% 
-  group_by(year) %>% 
-  summarize(quest = median(n_ratings), year = year)
-z[which.max(z$quest),]
+  summarize(n = n(), years = 2018 - first(year),
+            title = title[1],
+            rating = mean(rating)) %>%
+  mutate(rate = n/years) %>%
+  top_n(25, rate) %>%
+  arrange(desc(rate))
+
+
+movielens %>% 
+  group_by(genres) %>% 
+  summarize(n = n(), av = mean(rating), se = sd(rating)/sqrt(n())) %>% 
+  filter(n >= 1000) %>% 
+  ggplot(aes(x = genres, y = av, ymin = av - 2*se, ymax = av + 2*se)) + 
+  geom_point() + 
+  geom_errorbar()
